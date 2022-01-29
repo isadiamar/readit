5d0a6aff15f9d166ccce8a51668818418af37f4a
Isabel Diaz <isabel.d.m@outlook.com>
2022-01-29 13:08:23 +0100

    Login validation #3

--- readit-frontend/src/app/login-form/login-form.component.ts
+++ readit-frontend/src/app/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+  submitDisabled:boolean = true;
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private _snackBar:MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+
+    this.loginForm.valueChanges.subscribe(_=>{
+      this.checkDisabled();
+    })
   }
 
+  checkDisabled(): void {
+    let email = this.loginForm.controls['email'].value;
+    let password = this.loginForm.controls['password'].value;
+    this.submitDisabled = email === '' || password === '' ;
+  }
+
+  submit() {
+    if (this.loginForm.valid) {
+      let message = 'Logged succesfully';
+      let action = 'close'
+      this._snackBar.open(message, action, {
+        duration: 1000
+      });
+
+      this.clearFields()
+    }
+  }
+
+  clearFields() {
+    this.loginForm.reset();
+    Object.keys(this.loginForm.controls).forEach(key => {
+      this.loginForm.get(key)?.setErrors(null);
+    })
+  }
 }
