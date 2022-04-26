30e2e038ea67189e1d46103b55bfa341277591fc
Isabel Diaz <isabel.d.m@outlook.com>
2022-04-26 16:56:02 +0200

    Story Form #10

--- readit-frontend/src/app/readit/story-form/story-form.component.ts
+++ readit-frontend/src/app/readit/story-form/story-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-story-form',
@@ -7,15 +7,41 @@ import {FormBuilder, FormGroup} from "@angular/forms";
   styleUrls: ['./story-form.component.css']
 })
 export class StoryFormComponent implements OnInit {
+  formNewStory: FormGroup;
+  selectedFile:string = "";
+  genre1Control= new FormControl('romance');
+  genre2Control= new FormControl('romance');
+  privacyControl = new FormControl('public');
+  statusControl = new FormControl('progress');
 
   constructor(fb:FormBuilder) {
+    this.formNewStory = fb.group({
+      title: new FormControl(''),
+      description: new FormControl(''),
+      genre1 : this.genre1Control,
+      genre2 : this.genre2Control,
+      privacy:this.privacyControl,
+      status:this.statusControl,
+      color: new FormControl(''),
+      cover: new FormControl('')
+    })
   }
 
   ngOnInit(): void {
   }
 
   upload(event:Event){
-    console.log(event)
+    console.log(event);
+    let pathName:String = this.formNewStory.controls['cover'].value
+    const arrPath = pathName.split("\\");
+    this.selectedFile = arrPath[arrPath.length - 1];
   }
 
+  submit() {
+    console.log(this.formNewStory)
+  }
+
+  clearFields() {
+    this.formNewStory.reset();
+  }
 }
