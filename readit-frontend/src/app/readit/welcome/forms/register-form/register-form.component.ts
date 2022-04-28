fac051f273f74d0d967d1f2e5d27acc3f826fffa
Isabel Diaz <isabel.d.m@outlook.com>
2022-04-28 20:38:01 +0200

    Validate password #10

--- readit-frontend/src/app/readit/welcome/forms/register-form/register-form.component.ts
+++ readit-frontend/src/app/readit/welcome/forms/register-form/register-form.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {MatSnackBar} from '@angular/material/snack-bar';
 import {Subscription} from "rxjs";
 import {WelcomeService} from "../../../shared/services/data.service";
 import {AuthService} from "../../../../core/auth.service";
@@ -17,6 +16,7 @@ export class RegisterFormComponent implements OnInit, OnDestroy {
   submitDisabled: boolean = true;
   message: string;
   subscription: Subscription;
+  errorMsg: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -30,19 +30,22 @@ export class RegisterFormComponent implements OnInit, OnDestroy {
     this.registerForm = this.formBuilder.group({
       username: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$")]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      password: new FormControl('', [Validators.required, Validators.pattern("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$")]),
       confirmPassword: new FormControl('', [Validators.required])
     });
 
     this.registerForm.valueChanges.subscribe(_ => {
-      this.checkDisabled()
+      this.checkDisabled();
+      this.showErrorMessage();
     });
 
+    this.showErrorMessage()
     this.subscription = this.data.currentMessage.subscribe(message => this.message = message)
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    this.clearFields();
   }
 
   checkDisabled(): void {
@@ -92,5 +95,11 @@ export class RegisterFormComponent implements OnInit, OnDestroy {
 
   newMessage(message: string) {
     this.data.changeMessage(message);
+  }
+
+  showErrorMessage(){
+    let pswField = this.registerForm.controls["password"];
+    this.errorMsg = pswField.dirty&&pswField.invalid;
+
   }
 }
