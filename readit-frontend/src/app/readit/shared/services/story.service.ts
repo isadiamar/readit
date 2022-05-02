8f42390eaa1134de51b317c9e111eb78afb3978f
Isabel Diaz <isabel.d.m@outlook.com>
2022-05-02 20:10:56 +0200

    Front getAll Service #12

--- readit-frontend/src/app/readit/shared/services/story.service.ts
+++ readit-frontend/src/app/readit/shared/services/story.service.ts
@@ -3,9 +3,6 @@ import {HttpService} from "../../../core/http.service";
 import {Story} from "../models/story.model";
 import {Observable, of} from "rxjs";
 import {EndPoints} from "../../../shared/end-points";
-import {Genre} from "../models/genre.enum";
-import {Privacy} from "../models/privacy.enum";
-import {Status} from "../models/status.enum";
 
 @Injectable({
   providedIn: 'root',
@@ -28,11 +25,7 @@ export class StoryService{
   }
 
   getAll():Observable<Story[]>{
-    return of([
-      {title:'First', description:"This is a first description",  genre1: Genre.COMEDY , genre2:Genre.DRAMA, privacy:Privacy.PUBLIC, status:Status.COMPLETE, username: 'Laura'},
-      {title:'Seconds', description:"This is a second a description",  genre1: Genre.DRAMA , genre2:Genre.ROMANCE, privacy:Privacy.PUBLIC, status:Status.COMPLETE, username: 'Laura'},
-
-  ]);
-
-}
+    return this.httpService
+      .get(EndPoints.STORIES);
+  }
 }
--- readit-frontend/src/app/shared/end-points.ts
+++ readit-frontend/src/app/shared/end-points.ts
@@ -2,5 +2,5 @@ import {environment} from "../../environments/environment";
 
 export class EndPoints{
   static BASE = environment.REST_CORE + '/api/private';
-  static STORIES = EndPoints.BASE + '/story';
+  static STORIES = EndPoints.BASE + '/stories';
 }
